f9d5036a447ecceb0a6e216d9b83244ee598b0b8
Tim <tjk315@gmail.com>
2018-10-03 00:25:25 -0400

    Initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>{{$title}}</h1>
+<p>
+  Lorem ipsum dolor amet neutra beard sriracha tofu post-ironic glossier semiotics migas bicycle rights put a bird on it. XOXO cold-pressed +1 hoodie venmo chartreuse ramps master cleanse pork belly put a bird on it chambray street art. Single-origin coffee chambray +1 vaporware chartreuse art party, artisan flannel wayfarers kale chips. Af vegan migas, affogato man braid pour-over butcher gochujang hoodie flannel austin deep v. Normcore hot chicken man braid hammock tilde photo booth fam raclette stumptown roof party live-edge pop-up.
+  <br>
+  Bicycle rights pabst crucifix, thundercats scenester pickled church-key taiyaki. Edison bulb migas man braid, single-origin coffee disrupt tousled street art brunch fanny pack banjo gluten-free truffaut literally kitsch. Twee meggings kitsch ugh XOXO banh mi organic pug thundercats. Succulents edison bulb celiac organic umami kitsch. Irony cardigan organic kogi succulents forage franzen retro cray selvage pinterest. Lomo glossier prism sustainable vexillologist.
+  <br>
+  PBR&B typewriter williamsburg, shoreditch church-key brooklyn semiotics fanny pack wolf cloud bread messenger bag you probably haven't heard of them cronut slow-carb. Kinfolk selfies tote bag roof party glossier small batch. Mumblecore banh mi poutine williamsburg PBR&B yr pitchfork hammock live-edge knausgaard prism fashion axe. Affogato squid seitan semiotics, banjo lo-fi meggings 3 wolf moon enamel pin VHS hexagon tacos blog XOXO blue bottle. Trust fund 3 wolf moon bushwick normcore snackwave asymmetrical put a bird on it. Biodiesel slow-carb lyft, humblebrag hella squid raclette four loko.
+</p>
+<p>
+  Lorem ipsum dolor amet neutra beard sriracha tofu post-ironic glossier semiotics migas bicycle rights put a bird on it. XOXO cold-pressed +1 hoodie venmo chartreuse ramps master cleanse pork belly put a bird on it chambray street art. Single-origin coffee chambray +1 vaporware chartreuse art party, artisan flannel wayfarers kale chips. Af vegan migas, affogato man braid pour-over butcher gochujang hoodie flannel austin deep v. Normcore hot chicken man braid hammock tilde photo booth fam raclette stumptown roof party live-edge pop-up.
+  <br>
+  Bicycle rights pabst crucifix, thundercats scenester pickled church-key taiyaki. Edison bulb migas man braid, single-origin coffee disrupt tousled street art brunch fanny pack banjo gluten-free truffaut literally kitsch. Twee meggings kitsch ugh XOXO banh mi organic pug thundercats. Succulents edison bulb celiac organic umami kitsch. Irony cardigan organic kogi succulents forage franzen retro cray selvage pinterest. Lomo glossier prism sustainable vexillologist.
+  <br>
+  PBR&B typewriter williamsburg, shoreditch church-key brooklyn semiotics fanny pack wolf cloud bread messenger bag you probably haven't heard of them cronut slow-carb. Kinfolk selfies tote bag roof party glossier small batch. Mumblecore banh mi poutine williamsburg PBR&B yr pitchfork hammock live-edge knausgaard prism fashion axe. Affogato squid seitan semiotics, banjo lo-fi meggings 3 wolf moon enamel pin VHS hexagon tacos blog XOXO blue bottle. Trust fund 3 wolf moon bushwick normcore snackwave asymmetrical put a bird on it. Biodiesel slow-carb lyft, humblebrag hella squid raclette four loko.
+</p>
+<p>
+  Lorem ipsum dolor amet neutra beard sriracha tofu post-ironic glossier semiotics migas bicycle rights put a bird on it. XOXO cold-pressed +1 hoodie venmo chartreuse ramps master cleanse pork belly put a bird on it chambray street art. Single-origin coffee chambray +1 vaporware chartreuse art party, artisan flannel wayfarers kale chips. Af vegan migas, affogato man braid pour-over butcher gochujang hoodie flannel austin deep v. Normcore hot chicken man braid hammock tilde photo booth fam raclette stumptown roof party live-edge pop-up.
+  <br>
+  Bicycle rights pabst crucifix, thundercats scenester pickled church-key taiyaki. Edison bulb migas man braid, single-origin coffee disrupt tousled street art brunch fanny pack banjo gluten-free truffaut literally kitsch. Twee meggings kitsch ugh XOXO banh mi organic pug thundercats. Succulents edison bulb celiac organic umami kitsch. Irony cardigan organic kogi succulents forage franzen retro cray selvage pinterest. Lomo glossier prism sustainable vexillologist.
+  <br>
+  PBR&B typewriter williamsburg, shoreditch church-key brooklyn semiotics fanny pack wolf cloud bread messenger bag you probably haven't heard of them cronut slow-carb. Kinfolk selfies tote bag roof party glossier small batch. Mumblecore banh mi poutine williamsburg PBR&B yr pitchfork hammock live-edge knausgaard prism fashion axe. Affogato squid seitan semiotics, banjo lo-fi meggings 3 wolf moon enamel pin VHS hexagon tacos blog XOXO blue bottle. Trust fund 3 wolf moon bushwick normcore snackwave asymmetrical put a bird on it. Biodiesel slow-carb lyft, humblebrag hella squid raclette four loko.
+</p>
+@endsection
