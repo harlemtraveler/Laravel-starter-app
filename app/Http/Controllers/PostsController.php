56e9838193072efb5d5076a484e2343056768b7e
Tim <tjk315@gmail.com>
2018-10-10 00:46:50 -0400

    Completed "Access Control" section

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,6 +9,16 @@ use DB;
 class PostsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -98,6 +108,12 @@ class PostsController extends Controller
     public function edit($id)
     {
       $post = Post::find($id);
+
+      // Check for correct user
+      if(auth()->user()->id !== $post->user_id) {
+        return redirect('/posts')->with('error', 'Unauthorized Page');
+      }
+
       return view('posts.edit')->with('post', $post);
     }
 
@@ -134,6 +150,11 @@ class PostsController extends Controller
     {
         $post = Post::find($id);
         $post->delete();
+
+        // Check for correct user
+        if(auth()->user()->id !== $post->user_id) {
+          return redirect('/posts')->with('error', 'Unauthorized Page');
+        }
 
         return redirect('/posts')->with('success', 'Post Removed');
     }
