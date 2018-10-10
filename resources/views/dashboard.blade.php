a87e741fc9d9b1197b4f1723d6b265b6c25e2803
Tim <tjk315@gmail.com>
2018-10-10 00:20:42 -0400

    Completed "Model Relationships" section

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,6 +19,29 @@
                     <br>
                     <br>
                     <h3>Your Blog Posts</h3>
+                    @if (count($posts) > 0)
+                      <table class="table table-striped">
+                        <thead class="thead-dark">
+                          <th>Title</th>
+                          <th></th>
+                          <th></th>
+                        </thead>
+                        @foreach ($posts as $post)
+                          <tr>
+                            <td>{{$post->title}}</td>
+                            <td><a href="/posts/{{$post->id}}/edit" class="btn btn-primary float-right">Edit</a></td>
+                            <td>
+                              {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST']) !!}
+                                {{Form::hidden('_method','DELETE')}}
+                                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                              {!! Form::close() !!}
+                            </td>
+                          </tr>
+                        @endforeach
+                      </table>
+                    @else
+                      <p>You have no posts</p>
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,9 +11,9 @@
     {!!$post->body!!}
   </div>
   <hr>
-  <small>Written on {{$post->created_at}}</small>
+  <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
   <hr>
-  <a href="/posts/{{$post->id}}/edit" class="btn btn-secondary">Edit</a>
+  <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
 
   {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right']) !!}
     {{Form::hidden('_method','DELETE')}}
